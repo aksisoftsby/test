ee89bb789e90af15c6c8a0c3433964522b3c6edf
indi programmer <30791021+aksisoftsby@users.noreply.github.com>
2017-09-21 20:51:46 +0700

    Update xdesktop.sh

--- xdesktop.sh
+++ xdesktop.sh
@@ -4,4 +4,6 @@ apt-get upgrade
 apt-get install xfce -y
 apt-get install task-xfce-desktop -y
 systemctl set-default graphical.target
+apt-get install xrdp vino -y
+apt-get remove tightvncserver
 #etc.
